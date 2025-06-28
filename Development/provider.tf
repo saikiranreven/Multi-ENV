a4d9833a90a30a7ff4517ce69ea9cf7df60157fa
saikiranreven <rsksislvgy@gmail.com>
2025-06-28 17:18:19 -0230

    connc

--- Development/provider.tf
+++ Development/provider.tf
@@ -9,3 +9,11 @@ provider "google" {
   project = "project-bct-463501"
   region  = "us-central1"
 }
+
+provider "kubernetes" {
+  host                   = "https://${google_container_cluster.dev.endpoint}"
+  cluster_ca_certificate = base64decode(google_container_cluster.dev.master_auth[0].cluster_ca_certificate)
+  token                  = data.google_client_config.default.access_token
+}
+
+data "google_client_config" "default" {}
