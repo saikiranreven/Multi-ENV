537279d5e4532a3b62d26ee5b9dd4db48aa4323d
saikiranreven <rsksislvgy@gmail.com>
2025-06-28 18:19:18 -0230

    config

--- Development/provider.tf
+++ Development/provider.tf
@@ -9,3 +9,19 @@ provider "google" {
   project = "project-bct-463501"
   region  = "us-central1"
 }
+
+# Retrieve an access token as the Terraform runner
+data "google_client_config" "provider" {}
+
+data "google_container_cluster" "my_cluster" {
+  name     = "my-cluster"
+  location = "us-central1"
+}
+
+provider "kubernetes" {
+  host  = "https://${data.google_container_cluster.my_cluster.endpoint}"
+  token = data.google_client_config.provider.access_token
+  cluster_ca_certificate = base64decode(
+    data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate,
+  )
+}
