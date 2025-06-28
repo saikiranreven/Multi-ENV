b28df306b155c64bf2bd63decd9df8aafa7c3d4c
saikiranreven <rsksislvgy@gmail.com>
2025-06-28 18:21:18 -0230

    err4

--- Development/gke.tf
+++ Development/gke.tf
@@ -1,26 +1,34 @@
-resource "google_container_cluster" "dev" {
-  name     = "dev-cluster"
-  location = "us-central1"
-  
-  node_pool {
-    node_config {
-      machine_type = "e2-small"
-      oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
-    }
-  }
-}
-
 resource "kubernetes_deployment" "app" {
   metadata {
     name = "hello-app"
+    labels = {
+      app = "hello"
+    }
   }
+
   spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "hello"
+      }
+    }
+
     template {
-      container {
-        image = "gcr.io/${var.project_id}/hello-app:latest"
-        name  = "web"
-        port {
-          container_port = 80
+      metadata {
+        labels = {
+          app = "hello"
+        }
+      }
+
+      spec {
+        container {
+          name  = "web"
+          image = "gcr.io/${var.project_id}/hello-app:latest"
+          port {
+            container_port = 80
+          }
         }
       }
     }
@@ -33,12 +41,12 @@ resource "kubernetes_service" "app" {
   }
   spec {
     type = "LoadBalancer"
+    selector = {
+      app = "hello"
+    }
     port {
       port        = 80
       target_port = 80
-    }
-    selector = {
-      app = kubernetes_deployment.app.metadata[0].labels.app
     }
   }
 }
